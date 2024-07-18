6bbf1e359c4b21c10598e24e3c0ffcd3a96e90f6
Mariano Beiró <mbeiro@fi.uba.ar>
2024-07-18 16:30:31 -0300

    ejercicio parquet

--- HDFS-Serialization/load_parquet_file.py
+++ HDFS-Serialization/load_parquet_file.py
@@ -2,19 +2,25 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 from pyarrow import fs
+from pyarrow import dataset
 
+# Leemos el .csv a un DataFrame de Pandas
 df = pd.read_csv('/tmp/trips_2024.csv', names=['id_recorrido', 'duracion_recorrido', 'fecha_origen_recorrido',
 'id_estacion_origen', 'nombre_estacion_origen', 'direccion_estacion_origen', 
 'long_estacion_origen', 'lat_estacion_origen', 'fecha_destino_recorrido', 'id_estacion_destino', 'nombre_estacion_destino', 'direccion_estacion_destino', 'long_estacion_destino', 'lat_estacion_destino', 'id_usuario', 'modelo_bicicleta', 'genero'])
 
+df.write_csv('/tmp/hola.cc')
+
+# Lo cargamos en memoria con PyArrow
 tabla_pyarrow = pa.Table.from_pandas(df)
 
 mi_hdfs = fs.HadoopFileSystem('namenode', port=8020, user='hadoop', replication=2)
 
-pq.write_to_dataset(
+# Guardamos la tabla de PyArrow en formato .parquet en nuestro HDFS
+pa.dataset.write_dataset(
     tabla_pyarrow,
     filesystem=mi_hdfs,
-    root_path='/',
-    basename_template="bicis{i}.parquet",
-    partitioning=["genero"]
+    base_dir='/bicis',
+    format='parquet',
+    basename_template="bicis{i}.parquet"
 )
