b7c6127a4cddaaf391a33b20db27d610a6202603
Mariano Beiró <mbeiro@fi.uba.ar>
2024-07-20 17:40:35 -0300

    practica procesamiento distribuido

---
+++ Procesamiento_Distribuido/spark_app_code.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext, SparkConf
+
+# Creamos un contexto de Spark
+sc = SparkContext('local', 'test')
+
+# Nos conectamos al cluster HDFS
+archivo = sc.textFile("hdfs://namenode:8020/trips_2024.csv")
+
+#################################################33
+# Modo 2
+# sc = SparkContext(master='yarn')
+# archivo = sc.textFile("/trips_2024.csv")
+#################################################33
+
+# Ejecutamos la tarea, en este caso contar la cantidad de líneas del archivo
+archivo.count()
