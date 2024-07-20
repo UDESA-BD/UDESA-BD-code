71ee78f0c72c4ee90b2daf3d6db2a8f02c4f689d
Mariano Beiró <mbeiro@fi.uba.ar>
2024-07-20 18:51:16 -0300

    docker procesamiento distribuido

--- Procesamiento_Distribuido/spark_app_code.py
+++ Procesamiento_Distribuido/spark_app_code.py
@@ -1,15 +1,18 @@
 from pyspark import SparkContext, SparkConf
 
+#################################################33
+# Modo 1
 # Creamos un contexto de Spark
-sc = SparkContext('local', 'test')
-
+# sc = SparkContext('local', 'test')
+#
 # Nos conectamos al cluster HDFS
-archivo = sc.textFile("hdfs://namenode:8020/trips_2024.csv")
+# archivo = sc.textFile("hdfs://namenode:8020/trips_2024.csv")
+#################################################33
 
 #################################################33
 # Modo 2
-# sc = SparkContext(master='yarn')
-# archivo = sc.textFile("/trips_2024.csv")
+sc = SparkContext(master='yarn')
+archivo = sc.textFile("/trips_2024.csv")
 #################################################33
 
 # Ejecutamos la tarea, en este caso contar la cantidad de líneas del archivo
