1f5a39db1aa3c949ae549798bc18efcad7f49393
Mariano Beiró <mbeiro@fi.uba.ar>
2024-10-28 10:01:13 -0300

    guia serializacion

--- HDFS-Serialization/load_parquet_file.py
+++ HDFS-Serialization/load_parquet_file.py
@@ -10,6 +10,7 @@ df = pd.read_csv('/tmp/recorridos_realizados_2024.csv', names=['id_recorrido', '
 'long_estacion_origen', 'lat_estacion_origen', 'fecha_destino_recorrido', 'id_estacion_destino', 'nombre_estacion_destino', 'direccion_estacion_destino', 'long_estacion_destino', 'lat_estacion_destino', 'id_usuario', 'modelo_bicicleta', 'genero'], header=1)
 
 # Eliminando '.' en duracion_recorrido para que Pandas pueda guardarlo como float.
+df['duracion_recorrido'] = [ re.sub('.0^', '', str(x)) for x in df['duracion_recorrido'] ]
 df['duracion_recorrido'] = df['duracion_recorrido'].str.replace('.', '')
 df.astype({'duracion_recorrido': 'float'})
 
