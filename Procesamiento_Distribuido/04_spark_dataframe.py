c08a3b6ca70ad63daaec574ee112e4db0daa18c0
Mariano Beiró <mbeiro@fi.uba.ar>
2024-11-11 10:33:37 -0300

    guia spark

--- Procesamiento_Distribuido/04_spark_dataframe.py
+++ Procesamiento_Distribuido/04_spark_dataframe.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions
+from pyspark.sql import functions
 
 spark = SparkSession.builder.master('local') \
     .appName('test') \
@@ -11,5 +12,6 @@ esquema = spark.read.parquet("hdfs://namenode:8020/bicis/1/bicis0.parquet").sche
 # Nos conectamos al dataset
 df = spark.read.load("hdfs://namenode:8020/bicis/*", format='parquet', schema=esquema)
 
-# Utilizamos Spark SQL, que nos brinda un DataFrame con numerosos métodos
-# Documentación: https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/dataframe.html
+# Utilizamos Spark SQL, que nos brinda un DataFrame con dos formas de realizar consultas:
+# - Utilizando SQL: https://spark.apache.org/docs/3.5.2/sql-getting-started.html#running-sql-queries-programmatically
+# - Utilizando métodos propios de DataFrames: https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/dataframe.html
